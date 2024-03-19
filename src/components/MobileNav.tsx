445ef2a97a9c5a73a1f679ea37b092ffb6516924
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-19 22:44:53 +0530

    mobile user login logout

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,9 +1,14 @@
-import { Menu } from "lucide-react";
+import { CircleUserRound, Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 export default function MobileNav() {
+
+    const { loginWithRedirect, isAuthenticated, user } = useAuth0()
+
     return (
         <Sheet>
             <SheetTrigger>
@@ -11,13 +16,33 @@ export default function MobileNav() {
             </SheetTrigger>
             <SheetContent className="space-y-3">
                 <SheetTitle>
-                    <span>Welcome To Swiggy</span>
+                    {
+                        isAuthenticated ? (
+                            <span className="flex items-center font-bold gap-2">
+                                <CircleUserRound className="text-orange-500" />
+                                {user?.name || user?.email}
+                            </span>
+                        ) : (
+                            <span>Welcome To Swiggy</span>
+                        )
+                    }
                 </SheetTitle>
                 <Separator />
-                <SheetDescription className="flex">
-                    <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+                <SheetDescription className="flex flex-col gap-4">
+                    {
+                        isAuthenticated ? (
+                            <MobileNavLinks />
+                        ) : (
+                            <Button
+                                className="flex-1 font-bold bg-orange-500"
+                                onClick={async () => await loginWithRedirect()}
+                            >
+                                Log In
+                            </Button>
+                        )
+                    }
                 </SheetDescription>
             </SheetContent>
-        </Sheet>
+        </Sheet >
     )
 }
