e9d7a8b1d7fb0c90f6ecf5914b43fca4ba0074ae
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-19 22:23:34 +0530

    login logout and dropdown for user

---
+++ src/components/UserMenu.tsx
@@ -0,0 +1,39 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { CircleUserRound } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+export default function UserMenu() {
+
+    const { user, logout } = useAuth0()
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
+                <CircleUserRound className="text-orange-500" />
+                {user?.name || user?.email}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <Link
+                        to="/user-profile"
+                        className="font-bold hover:text-orange-500"
+                    >
+                        User Profile
+                    </Link>
+                </DropdownMenuItem>
+                <Separator />
+                <DropdownMenuItem>
+                    <Button 
+                    className="flex flex-1 font-bold bg-orange-500"
+                    onClick={async () => await logout()}
+                    >
+                        Log Out
+                    </Button>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
