6a25a2a424a5d2a9e00e2392531015b2b1466909
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-20 22:58:44 +0530

    user profile update form created

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,13 +2,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Layout><HomePage /></Layout>} />
+            <Route path="/" element={<Layout showHero={true}><HomePage /></Layout>} />
             <Route path="/auth-callback" element={<AuthCallbackPage />} />
-            <Route path="/user-profile" element={<span>User Profile</span>} />
+            <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     )
---
+++ src/pages/UserProfilePage.tsx
@@ -0,0 +1,5 @@
+import UserProfileForm from "@/forms/UserProfileForm";
+
+export default function UserProfilePage() {
+    return <UserProfileForm />
+}
