97696c6665340ae6a7bc80cc9ac76c35b9547f9e
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-22 22:03:41 +0530

    useCreateRestaurant hook created

---
+++ src/api/RestaurantApi.tsx
@@ -0,0 +1,48 @@
+import { Restaurant } from "@/types"
+import { useAuth0 } from "@auth0/auth0-react"
+import axios from "axios"
+import { useMutation } from "react-query"
+import { toast } from "sonner"
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string
+
+export const useCreateRestaurant = () => {
+    const { getAccessTokenSilently } = useAuth0()
+
+    const createRestaurantRequest = async (restaurant: FormData): Promise<Restaurant> => {
+        const accesstoken = await getAccessTokenSilently()
+        const response = await axios.post(`${API_BASE_URL}/api/restaurant`, restaurant, {
+            headers: {
+                Authorization: `Bearer ${accesstoken}`
+            }
+        })
+        if (!response.data) {
+            throw new Error('Error creating restaurant')
+        }
+        return response.data
+    }
+
+    const {
+        mutateAsync: createRestaurant,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useMutation(createRestaurantRequest)
+
+    if (isSuccess) {
+        toast.success('Restaurant created successfully')
+    }
+    if (error) {
+        toast.error('Error creating restaurant')
+    }
+
+    return {
+        createRestaurant,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }
+
+}
