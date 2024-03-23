3393f2af7e0c64a9311c8307408e28570ecfba09
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-23 21:02:26 +0530

    search result info component

---
+++ src/components/SearchResultInfo.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+type Props = {
+    total: number
+    city: string
+}
+
+export default function SearchResultInfo({ total, city }: Props) {
+    return (
+        <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
+            <span>
+                {total} restaurants found in {city}
+                <Link to="/" className="text-orange-500 underline text-sm font-semibold cursor-pointer ml-2">Change Location</Link>
+            </span>
+            data here
+        </div>
+    )
+}
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,9 +1,28 @@
+import { useSearchRestaurants } from "@/api/SearchApi";
+import SearchResultInfo from "@/components/SearchResultInfo";
 import { useParams } from "react-router-dom";
 
 export default function SearchPage() {
-    const {city} = useParams()
+
+    const { city } = useParams()
+    const { restaurants, isLoading } = useSearchRestaurants(city)
+
+    if (!restaurants?.data || !city) {
+        return <span>No results found...</span>
+    }
+
+    if (isLoading) {
+        return <span>Loading...</span>
+    }
 
     return (
-        <span>Search Page for {city}</span>
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+            <div id="cuisine-list">
+                cuisine list
+            </div>
+            <div id="main-content" className="flex flex-col gap-5">
+                <SearchResultInfo total={restaurants.pagination.total} city={city} />
+            </div>
+        </div>
     )
 }
