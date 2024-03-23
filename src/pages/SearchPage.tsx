c0f5fe282a178615218b5504bfd5f9a7929e56d6
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-23 21:38:21 +0530

    search card component

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchRestaurants } from "@/api/SearchApi";
+import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
 import { useParams } from "react-router-dom";
 
@@ -22,6 +23,10 @@ export default function SearchPage() {
             </div>
             <div id="main-content" className="flex flex-col gap-5">
                 <SearchResultInfo total={restaurants.pagination.total} city={city} />
+                {restaurants.data.map((restaurant) => (
+                    <SearchResultCard restaurant={restaurant} />
+                ))
+                    }
             </div>
         </div>
     )
